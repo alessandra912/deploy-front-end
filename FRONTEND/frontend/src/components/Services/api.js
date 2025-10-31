a42488a4074888c385eea8ff369bfe9484d6e309
alessandra912 <ssantossilva241@gmail.com>
2025-10-31 01:23:01 -0300

    Delete FRONTEND directory

--- FRONTEND/frontend/src/components/Services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// Configuração da instância do Axios para a comunicação com a API
-const api = axios.create({
-    baseURL: 'http://localhost:3000', // Substitua pela URL da sua API
-});
-
-export default api;
